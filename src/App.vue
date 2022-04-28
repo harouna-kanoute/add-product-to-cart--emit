8373f2c043009d6d6fde5cc5ed12c5d12e7b2daa
Harouna Kanouté <44778626+harouna-kanoute@users.noreply.github.com>
2022-04-28 15:00:34 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <h1>product</h1>
+    <cart @addcart="updatecart" @delete-cart="deleteCart" />
+    <p class="cart">Panier ({{ panier }})</p>
+  </div>
+</template>
+
+<script>
+import cart from "./components/cart.vue";
+export default {
+  name: "App",
+  components: {
+    cart,
+  },
+  data() {
+    return {
+      panier: 0,
+    };
+  },
+  methods: {
+    updatecart: function () {
+      this.panier += 1;
+    },
+    deleteCart: function () {
+      if (this.panier > 0) {
+        this.panier -= 1;
+      } else {
+        alert("le panier est vide !");
+      }
+    },
+  },
+};
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  position: relative;
+}
+.cart {
+  position: absolute;
+  top: 2px;
+  right: 2px;
+}
+</style>
---
+++ src/components/cart.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="product">
+    <div class="product-details">
+      <img src="../assets/index.jpeg" alt="" />
+      <div>
+        <h2>{{ shoesName }}</h2>
+        <p>{{ detail }}</p>
+        <p>{{ price }} €</p>
+
+        <button @click="addTocart">ajouter au panier</button>
+        <button @click="initial">vider le panier</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "cart",
+
+  data() {
+    return {
+      shoesName: "Nike Air Force 1 High",
+      detail: "Chaussures personnalisables pour Homme",
+      price: 149.99,
+    };
+  },
+
+  methods: {
+    addTocart: function () {
+      this.$emit("addcart");
+    },
+    initial: function () {
+      this.$emit("delete-cart");
+    },
+  },
+};
+</script>
+
+<style>
+body {
+  background: none;
+}
+.product {
+}
+.product-details {
+  display: flex;
+  justify-content: space-between;
+  padding: 0px;
+  margin: 20px;
+}
+.product-details div {
+  text-align: left;
+  padding: 0px 10px;
+  margin: 0px;
+}
+.product-details img {
+  width: 40%;
+  background: none;
+}
+h2 {
+  color: #d35400;
+}
+button {
+  transition: 0.2s;
+  font-size: 1em;
+  border: none;
+  border-radius: 3px;
+  height: 40px;
+  background-color: black;
+  color: white;
+  cursor: pointer;
+  margin: 5px;
+  width: 150px;
+}
+button:hover {
+  transition: 0.2s;
+  font-size: 1.1em;
+  border: none;
+  border-radius: 5px;
+  height: 40px;
+  background-color: #d35400;
+  color: white;
+  cursor: pointer;
+}
+</style>
